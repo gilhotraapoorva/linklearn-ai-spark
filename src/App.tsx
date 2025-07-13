a9a9a29b6bdef0b369fe21eaf9d8f6559e348c2d
Sriteja Pashya <sritejapashya@gmail.com>
2025-07-13 07:00:42 +0530

    weekly hackathon page (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import HackathonDetails from "./pages/HackathonDetails";
 import NotFound from "./pages/NotFound";
 import QuestPage from "./pages/DailyQuest";
 
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/quest/:questId" element={<QuestPage />} />
+          <Route path="/hackathon/:id" element={<HackathonDetails />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
